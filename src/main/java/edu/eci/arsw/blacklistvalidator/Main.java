1eb9bd47548070b4288d1d9598b2a30b5c181ca4
juan.daza-p <juanpadazer15@gmail.com>
2025-02-27 09:25:34 -0500

    :(

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Main {
     public static void main(String a[]){
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        boolean blackListOcurrences=hblv.checkHost("200.24.34.55", 1);
+        int blackListOcurrences=hblv.checkHost("200.24.34.55", 1);
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
     }
 }
